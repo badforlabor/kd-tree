4d13369bda2c2403a053dfeebdde85aeebb21eca
badlabor <badlabor@work.com>
2017-11-29 18:22:38 +0800

    123

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # kd-tree
+详情见
 https://home.wlu.edu/~levys/software/kd/
+
+## 例子
+
+按照官网，写了一个简单示例。
---
+++ main.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KDTreeDLL;
+
+namespace kdtree
+{
+    class main
+    {
+        static void Main()
+        {
+            double[] A = { 2, 5 };
+            double[] B = { 1, 1 };
+            double[] C = { 3, 9 };
+            double[] T = { 1, 10 };
+
+            var kd = new KDTree(2);
+            try
+            {
+                kd.insert(A, ("A"));
+                kd.insert(B, ("B"));
+                kd.insert(C, ("C"));
+            }
+            catch (Exception)
+            {
+                
+            }
+
+            // 查找
+            Console.WriteLine(kd.search(B));
+
+            // 寻找最近的一个
+            Console.WriteLine(kd.nearest(B));
+
+            // 寻找附近的N个
+            var ret = kd.nearest(B, 3);
+            foreach (var it in ret)
+            {
+                Console.WriteLine(it);
+            }            
+
+            Console.WriteLine("按任意键退出...");
+            Console.ReadKey();
+        }
+    }
+}
